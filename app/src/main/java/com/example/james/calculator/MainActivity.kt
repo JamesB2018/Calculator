8d8da841e34501fd8ff40c212c8a4b815ef9cb63
James <jmb47580@ucmo.edu>
2018-06-30 23:22:27 -0500

    adding calculations

--- app/src/main/java/com/example/james/calculator/MainActivity.kt
+++ app/src/main/java/com/example/james/calculator/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.james.calculator
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+    val operationList: MutableList<String> = arrayListOf()
+    val numberCache: MutableList<String> = arrayListOf()
+
+    fun makeString(list: List<String>,joiner: String = "") : String {
+
+        if (list.isEmpty()) return ""
+        return list.reduce { r, s -> r + joiner + s }
+    }
+    fun updateDisplay(mainDisplayString: String){
+
+        val fullCalculationString = makeString(operationList, " ")
+        var fullCalculationTextView = findViewById<TextView>(R.id.tvNumber)
+        fullCalculationTextView.text = fullCalculationString
+
+        val mainTextView = findViewById(R.id.tvResult) as TextView
+        mainTextView.text = mainDisplayString
+    }
+    fun numberClick(view: View){
+        val button = view as Button
+        val numberString = button.text;
+
+        val text = makeString(numberCache)
+        updateDisplay(text)
+    }
+
 }
